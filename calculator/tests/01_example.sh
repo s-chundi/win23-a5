f0ae6a3239f68dd9a6fb95b2a55a21e008db42ef
Suhas Chundi <suhaschundi@gmail.com>
2024-02-26 18:59:43 -0800

    makefile and tests

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -29,3 +29,15 @@ if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. succe
   echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
   exit 1
 fi
+
+# Test 04: Ensure simple case has correct output...
+if [[ $($CALCULATOR 6 '*' 13) -ne 78 ]]; then  # If the output of the program is not 2...
+  echo 'ERROR! A valid run of the calculator (1 + 1) failed to produce 2 as an output!'
+  exit 1
+fi
+
+# Test 05: Division test
+if [[ $($CALCULATOR 16 / 4) -ne 4 ]]; then  # If the output of the program is not 2...
+  echo 'ERROR! A valid run of the calculator (1 + 1) failed to produce 2 as an output!'
+  exit 1
+fi
